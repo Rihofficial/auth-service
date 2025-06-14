990e6181a85ca0ef98e6d60b1e56090954a5ab18
Abejoye Timothy <98910872+Rihofficial@users.noreply.github.com>
2025-06-14 19:52:53 +0100

    second

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');  
-const  helmet = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 require("dotenv").config();
 
 const limiter = require("./middleware/rateLimiter");
-
 
 const app = express();
 
@@ -14,7 +13,7 @@ app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -22,22 +21,18 @@ app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/protected", require("./routes/protectedRoute"));
 app.use("/api/user", require("./routes/userRoutes"));
 
-
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Connected to MongoDB");
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
-}   
+};
 startServer();
